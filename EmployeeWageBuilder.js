abf11a591b5b65be03785941d9d207b92a26b098
harshi0806 <harshitashrivatav121@gmail.com>
2020-09-10 17:14:27 +0530

    ADDED: Code to calculate Daily Employee Wage

--- EmployeeWageBuilder.js
+++ EmployeeWageBuilder.js
@@ -1,8 +1,8 @@
 const IS_PRESENT = 1;
 const WAGE_PER_HOUR = 20;
 var empAttendance = Math.floor(Math.random() * 10) % 2;
-var workingHours;
-function calculateAttendance() {
+var workingHours, empWage;
+function calculateDailyWage() {
     if (empAttendance == IS_PRESENT) {
         console.log("Employee is Present.");
         workingHours = 8;
@@ -11,6 +11,7 @@ function calculateAttendance() {
         console.log("Employee is Absent.");
         workingHours = 0;
     }
-    return workingHours;
+empWage = workingHours * WAGE_PER_HOUR; 
+return empWage;
 }
-console.log(calculateAttendance());
+console.log(calculateDailyWage());
