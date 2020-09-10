5de3dbd991c520d309002839b09b4fd7ce9b8dea
harshi0806 <harshitashrivatav121@gmail.com>
2020-09-10 18:03:36 +0530

    REFACTORED: Code using Switch case statement

--- EmployeeWageBuilder.js
+++ EmployeeWageBuilder.js
@@ -4,17 +4,21 @@ const WAGE_PER_HOUR = 20;
 var empAttendance = Math.floor(Math.random() * 10) % 3;
 var workingHours, empWage;
 function calculateDailyWage() {
-    if (empAttendance == IS_FULL_TIME) {
-        console.log("Employee is Present for Full time.");
-        workingHours = 8;
-    }
-    else if (empAttendance == IS_PART_TIME){
-        console.log("Employee is Present for Part time.");
-        workingHours = 4;
-    }
-    else {
-        console.log("Employee is Absent.");
-        workingHours = 0;
+    switch (empAttendance) {
+        case IS_FULL_TIME : {
+            console.log("Employee is Present for Full time.");
+            workingHours = 8;
+            break;
+        }
+        case IS_PART_TIME : {
+            console.log("Employee is Present for Part time.");
+            workingHours = 4;
+            break;
+        }
+        default : {
+            console.log("Employee is Absent.");
+            workingHours = 0;
+        }
     }
 empWage = workingHours * WAGE_PER_HOUR; 
 return empWage;
